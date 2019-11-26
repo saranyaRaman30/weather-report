5ab1540f96b7193ce001d225bcb02a2cde61e6f8
saranyaRaman30 <58113803+saranyaRaman30@users.noreply.github.com>
2019-11-26 17:55:36 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <weather></weather>
+<!--     <test></test> -->
+  </div>
+</template>
+
+<script>
+import weather from './components/weather.vue'
+// import test from './components/test.vue'
+export default {
+  name: 'app',
+   components:{
+    weather
+  }
+  
+}
+</script>
---
+++ src/components/weather.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="weather-container">
+  	<div class="weather-textbox">
+	  	<input type="text" placeholder="Search.." name="search2" ref ="textvalue" v-on:keyup.enter="getweather">
+        <button type="submit" @click="getweather" ><i class="fa fa-search"></i></button>
+	 </div>
+	 <div class="weather-display-section">
+	 	<div class="show-weather-details">
+	 		<!-- <p>{{listOfObjects}}</p> -->
+	 		<!-- <p v-for="(value, key) in posts">{{ key }}</p> -->
+	 		<ul>
+		      <li v-for="(value, key) in listOfObjects">{{ key }}: {{ value }} </li>
+		    </ul>
+	 		
+	 	</div>
+	 	
+	 </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'weather',
+ data () {
+    return {
+     items: null,
+     errors: [],
+     listOfObjects: null
+    }
+  },
+  methods:{
+  	getweather(){
+  		console.log(this.$refs.textvalue.value);
+  		const searchkeyword = this.$refs.textvalue.value;
+        const apikey = 'e0b6267576917bb1dc04e0237c9d009e';
+  		axios.get(`http://api.weatherstack.com/current?access_key=${apikey}&query=${searchkeyword}`)
+  		// .then(response => {
+    //               this.items = () => JSON.parse(response.data);
+    //               console.log('erere',this.items);
+    //     })
+	    .then(response => {
+	      this.info = response.data.location;
+	       
+		    })
+     //   let listOfObjects = Object.keys(response.data.location).map((value,index) => {
+     //      return response.data.location[value]
+		   //  console.log('erere',listOfObjects);
+	    // })
+	    .catch(e => {
+	      this.errors.push(e)
+	    })
+
+  	}
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import '../styles/weather.scss';
+</style>
